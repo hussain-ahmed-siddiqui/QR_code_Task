591540ea36467d36e7e75967c02428ca2e0bac72
hussain-ahmed-siddiqui <hussain.siddiqui.5th@gmail.com>
2023-12-06 16:14:18 +0500

    Put both tasks in one file
    I put both p2p and p2m task execution in one main function

--- QR code decryption/untitled/src/main/java/org/example/P2P_Template.java
+++ QR code decryption/untitled/src/main/java/org/example/P2P_Template.java
@@ -1,12 +1,18 @@
 package org.example;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class P2P_Template {
 
-    HashMap<Integer, String> dictionary = new HashMap<>();
+    Map<Integer, String> dictionary;
 
-    P2P_Template(){
+    public P2P_Template(){
+        this.dictionary = new HashMap<>();
+        initializeTagInfoMap();
+    }
+
+    private void initializeTagInfoMap() {
         dictionary.put(0,"Payload Format Indicator");
         dictionary.put(1,"Point of Initiation Method");
         dictionary.put(2,"Scheme Identifier");
@@ -19,4 +25,43 @@ public class P2P_Template {
         dictionary.put(9,"Reserved for Future Use");
         dictionary.put(10,"CRC");
     }
+
+    public void parse(String input ) {
+        P2P_Template template = new P2P_Template();
+        boolean []tag_ids = new boolean[11];
+        int [] mandatory_ids={0,1,2,4,10};
+        int startIndex = 0;
+        int endIndex =2;
+        int length;
+        int key;
+        try{
+            for(int i=0;endIndex<input.length();i++){
+                key = Integer.parseInt(input.substring(startIndex,endIndex));
+                System.out.print(template.dictionary.get(key)+": ");
+                tag_ids[key]=true;
+                startIndex=endIndex;
+                endIndex+=2;
+                length=Integer.parseInt( input.substring(startIndex,endIndex));
+                startIndex=endIndex;
+                System.out.println(input.substring(startIndex,endIndex+length));
+                startIndex=endIndex+length;
+                endIndex=startIndex+2;
+            }
+        }catch (StringIndexOutOfBoundsException e){
+
+            System.out.println("\nQR code is corrupted or incomplete!");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        boolean flag = true;
+        for(int i=0;i<mandatory_ids.length;i++){
+            if(!tag_ids[mandatory_ids[i]]){
+                flag=false;
+                System.out.println("\nQR code is corrupted or incomplete!");
+                break;
+            }
+        }
+
+    }
+
 }
--- QR code decryption/untitled/src/main/java/org/example/TagParser.java
+++ QR code decryption/untitled/src/main/java/org/example/TagParser.java
@@ -1,6 +1,9 @@
 package org.example;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 
 public class TagParser {
 
@@ -138,9 +141,10 @@ public class TagParser {
     }
 
 
-    public static void main(String[] args) {
-        TagParser parser = new TagParser();
-        parser.parseString("00020101021202164000123456789012041653451234567890121516634512345678901227420010A0000007360124PK73FAYS35553900000000005204599953035865802PK5916AZIZGeneralStore6007KARACHI62890117234567854321234560211032400000000316AZIZGeneralStore05131238765432123071245678909876563045376"); // Example input
-        parser.showInfo();
-    }
+
+//    public static void main(String[] args) {
+//        TagParser parser = new TagParser();
+//        parser.parseString("00020101021202164000123456789012041653451234567890121516634512345678901227420010A0000007360124PK73FAYS35553900000000005204599953035865802PK5916AZIZGeneralStore6007KARACHI62890117234567854321234560211032400000000316AZIZGeneralStore05131238765432123071245678909876563045376"); // Example input
+//        parser.showInfo();
+//    }
 }
